034e0805da2d830ed02a931d6cd084479ebe6acf
Alexandre Pereira <alexandre.pereira@webtraining.int>
2019-01-16 09:50:11 -0200

    Projeto desenvolvido junto ao curso de C# da Alura, explorando a importação de dados de um arquivo externo através do controle de buffer e do StreamReader.

---
+++ CSharp_ImportacaoDados/1-TratandoEntradasDiretamente.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_ImportacaoDados
+{
+    partial class Program
+    {
+        static void TratandoEntradasDiretamente()
+        {
+            var caminhoArquivo = "contas.txt"; // Arquivo Origem dos dados importados.
+
+            using (var fluxoArquivo = new FileStream(caminhoArquivo, FileMode.Open)) // USING que implementa método Disposable, que ao final da execução fecha o arquivo utilizado.
+            {
+                var buffer = new byte[1024]; // Objeto de controle de dados carregados por vez.
+                var numBytesLidos = -1;
+
+                while (numBytesLidos != 0)
+                {
+                    numBytesLidos = fluxoArquivo.Read(buffer, 0, buffer.Length); // Identificar e determinar quais e quantos bytes serão lidos do arquivo de origem.
+                    EscreverBuffer(buffer, numBytesLidos);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Decodificar os dados byte em texto e escrever na tela do Console.
+        /// </summary>
+        /// <param name="buffer">Dados de origem, do tipo 'byte'</param>
+        /// <param name="numBytesLidos">Número de bytes que serão lidos dos dados de origem</param>
+        static void EscreverBuffer(byte[] buffer, int numBytesLidos)
+        {
+            var utf8 = new UTF8Encoding(); // Objeto de Encoding para transformar os bytes em string.
+
+            var texto = utf8.GetString(buffer, 0, numBytesLidos);
+            Console.Write(texto);
+        }
+    }
+}
---
+++ CSharp_ImportacaoDados/Program.cs
@@ -0,0 +1,57 @@
+﻿using BankOne;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_ImportacaoDados
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            var caminhoArquivo = "contas.txt"; // Arquivo Origem dos dados importados.
+
+            using (var fluxoArquivo = new FileStream(caminhoArquivo, FileMode.Open)) // USING que implementa método Disposable, que ao final da execução fecha o arquivo utilizado.
+            using (var leitor = new StreamReader(fluxoArquivo)) // Criar objeto de controle de leitura.
+            {
+                while (!leitor.EndOfStream) // Continuar até todos os dados serem lidos.
+                {
+                    var linha = leitor.ReadLine(); // Ler uma linha por vez.
+                    var contaCorrente = ConverterStringParaContaCorrente(linha); // Variável que representa um Objeto de ContaCorrente.
+
+                    var mensagem = $"Cliente: {contaCorrente.Titular.Nome} - Ag/Conta nº: {contaCorrente.Agencia}/{contaCorrente.Numero} - Saldo: {contaCorrente.Saldo} _____";
+                    Console.WriteLine(mensagem);
+                }
+            }          
+
+            Console.ReadLine();
+        }       
+
+        /// <summary>
+        /// Criar um Objeto do tipo ContaCorrente.
+        /// </summary>
+        /// <param name="linha">Linha de dados lida do arquivo de Origem</param>
+        /// <returns></returns>
+        static ContaCorrente ConverterStringParaContaCorrente(string linha)
+        {
+            var campos = linha.Split(','); // Coletar os dados separados da linha do arquivo de origem.
+
+            var agencia = int.Parse(campos[0]);
+            var numero = int.Parse(campos[1]);
+            var saldo = double.Parse(campos[2].Replace(".",","));
+            var nome = campos[3];
+
+            var titular = new Cliente(); // Criar e atribuir o Objeto do tipo Cliente.
+            titular.Nome = nome;
+
+            var resultado = new ContaCorrente(agencia, numero); // Criar e atribuir o Objeto do tipo ContaCorrente.
+            resultado.Depositar(saldo);
+            resultado.Titular = titular;
+
+            return resultado; // Retornar o Objeto completo.
+        }
+    }
+}
